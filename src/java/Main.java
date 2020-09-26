1443edb50a8eca811e0129fbe2a703f07ff2a31c
Quyunshuo <quyunshuo@qq.com>
2020-09-26 10:38:44 +0800

    Initial commit

---
+++ src/java/Main.java
@@ -0,0 +1,10 @@
+package java;
+
+/**
+ * Java 算法入口程序
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/kotlin/Main.kt
@@ -0,0 +1,8 @@
+package kotlin
+
+/**
+ * Kotlin 算法入口程序
+ */
+fun main() {
+
+}
